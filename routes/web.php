b6a64b709765f4d110707ce1f42242af647133c7
etmjr03 <etmjr03@gmail.com>
2024-06-18 01:01:57 -0300

    Testando artisan para criar Controllers e Models e aprendendo a usar rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuario/{id}/{nome?}', function ($id, $nome = 'Anônimo') {
+    return 'Usuário '.$nome.' - id: '.$id;
+});
